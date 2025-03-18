444747ad4a80337294a77bd567a1afad52cca45b
krishna <krishnasingj137333@gmail.com>
2025-03-18 23:29:03 +0530

    updated request

--- Patient-Service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ Patient-Service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -2,6 +2,7 @@ package com.pm.patientservice.controller;
 
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
 import com.pm.patientservice.service.PatientService;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
@@ -29,7 +30,7 @@ public class PatientController {
     }
 
     @PostMapping("/save")
-    public ResponseEntity<PatientResponseDTO> createPatient(@Valid  @RequestBody PatientRequestDTO patientRequestDTO) {
+    public ResponseEntity<PatientResponseDTO> createPatient(@Validated({Default.class, CreatePatientValidationGroup.class})  @RequestBody PatientRequestDTO patientRequestDTO) {
         return new ResponseEntity<>(patientService.createPatient(patientRequestDTO), HttpStatus.CREATED);
     }
 
